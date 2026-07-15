04cd7a8f4b6d7923c997cfc4391be239f1d39212
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-15 10:49:51 +0530

    ✅ While Loop | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-15

---
+++ gfg/basic/while-loop/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void utility(int x) {
+        // code here 
+        int i=0;
+        while(x>=0){
+            System.out.print(x+" ");
+            x--;
+        }
+    }
+}
