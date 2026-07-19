67b53325d9d974547dfe9b16250c9d6f32dca012
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-19 10:40:08 +0530

    ✅ find-index-1614919939--145853 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-19

---
+++ gfg/medium/find-index-1614919939--145853/solution.py
@@ -0,0 +1,11 @@
+arr = tuple(map(int, input().split()))
+x = int(input())
+
+for i in arr:
+    if x==i:
+        print(arr.index(i))
+    
+########### Write your code below ###############
+# Print the index of x in arr
+
+########### Write your code above ###############
