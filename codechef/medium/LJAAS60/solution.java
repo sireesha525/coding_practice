46193c386a2e2cee81ccd8b657f5436741a0ae06
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-22 11:34:33 +0530

    ✅ LJAAS60 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ codechef/medium/LJAAS60/solution.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+	Scanner sc=new Scanner(System.in);
+	int marks1=sc.nextInt();
+	if(marks1>90){
+	    System.out.println("A");
+	}
+	else if(marks1>70){
+	    System.out.println("B");
+	}
+	else if(marks1>=40){
+	    System.out.println("C");
+	}
+	else{
+	    System.out.println("F");
+	}
+	int marks2=sc.nextInt();
+	if(marks2>90){
+	    System.out.println("A");
+	}
+	else if(marks2>70){
+	    System.out.println("B");
+	}
+	else if(marks2>=40){
+	    System.out.println("C");
+	}
+	else{
+	    System.out.println("F");
+	}
+	int marks3=sc.nextInt();
+	if(marks3>90){
+	    System.out.println("A");
+	}
+	else if(marks3>70){
+	    System.out.println("B");
+	}
+	else if(marks3>=40){
+	    System.out.println("C");
+	}
+	else{
+	    System.out.println("F");
+	}    
+	}
+}
