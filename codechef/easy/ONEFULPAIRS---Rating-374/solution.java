c73d39ee41a8afd4ebc3fbc7ef7a205e825693bd
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-23 11:07:44 +0530

    ✅ ONEFULPAIRS - Rating 374 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/easy/ONEFULPAIRS---Rating-374/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		// write your code here
+		if(a+b+(a*b)==111){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
