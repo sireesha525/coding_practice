b9b5f3a9fb4cc23e0a60cc535fc44cb6c70d4cd2
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-23 11:17:35 +0530

    ✅ LJAAS109 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/medium/LJAAS109/solution.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here 
+	int num=1;
+	while (num<=5){
+	    System.out.println(num*num+" ");
+	    num++;
+	}
+	}
+}
