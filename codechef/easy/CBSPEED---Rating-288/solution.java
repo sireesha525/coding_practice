5c62ddcd08630775d5b18238f7622a82a73b9e7f
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-23 10:59:27 +0530

    ✅ CBSPEED - Rating 288 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-23

---
+++ codechef/easy/CBSPEED---Rating-288/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		// write your code here
+		if (y>x){
+		System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
