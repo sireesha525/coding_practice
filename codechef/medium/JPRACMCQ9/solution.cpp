0379bac95c1183370b5ec347f8f93d05bed0789f
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-23 11:03:58 +0530

    ✅ JPRACMCQ9 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-23

--- codechef/medium/JPRACMCQ9/solution.cpp
+++ codechef/medium/JPRACMCQ9/solution.cpp
@@ -8,15 +8,10 @@ class Codechef
 	public static void main (String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		
 		// write your code here
-		if (x>=2*y){
-		    System.out.println("Yes");
-		}
-		else{
-		    System.out.println("No");
-		}
+		
 	}
 }
