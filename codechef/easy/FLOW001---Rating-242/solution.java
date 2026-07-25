925ae9df3eed138574f05e7e3f735298319fc0e3
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-25 20:15:38 +0530

    ✅ FLOW001 - Rating 242 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-25

---
+++ codechef/easy/FLOW001---Rating-242/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0)
+		{
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		// write your code here
+    		System.out.println(a+b);
+		}
+		
+	}
+}
