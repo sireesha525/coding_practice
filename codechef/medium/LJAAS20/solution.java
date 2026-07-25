b076858fb8b88ccd66349163b64d9e4829b25a0e
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-25 20:31:24 +0530

    ✅ LJAAS20 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-25

---
+++ codechef/medium/LJAAS20/solution.java
@@ -0,0 +1,14 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		
+		System.out.println(1+"-"+(1*1));
+		System.out.println(2+"-"+(2*2));
+		System.out.println(3+"-"+(3*3));
+		System.out.println(4+"-"+(4*4));
+		System.out.println(5+"-"+(5*5));
+
+	}
+}
