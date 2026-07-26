02df513acfef60d7f2c5875f46b3ac633239c77e
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-26 10:33:13 +0530

    ✅ BMJ210 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-26

---
+++ codechef/medium/BMJ210/solution.java
@@ -0,0 +1,17 @@
+// Update the code below to solve the problem
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner read = new Scanner(System.in);
+		
+		int t = read.nextInt();
+		for(int i=0;i<t;i++){
+		int x = read.nextInt();
+		int y=2*x*5;
+		System.out.println(y);
+		}
+	}
+}
