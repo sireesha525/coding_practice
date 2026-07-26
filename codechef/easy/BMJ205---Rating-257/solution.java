00d52c0c425ab58b82885d2f4d559390bb9d1b37
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-26 10:24:22 +0530

    ✅ BMJ205 - Rating 257 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-26

---
+++ codechef/easy/BMJ205---Rating-257/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner read = new Scanner(System.in);
+		
+		int t = read.nextInt();
+		for(int i=0; i<t; i++)
+		{
+            // Accept 2 integers inputs
+        	int x = read.nextInt();
+        	int y = read.nextInt();
+        	System.out.println(x*y);
+            // Update your code below this line to solve the problem
+
+		}
+	}
+}
