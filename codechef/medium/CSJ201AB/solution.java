fb7b144645fd2f956101f4dad86b6b6a62e3047f
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-28 11:07:03 +0530

    ✅ CSJ201AB | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-28

---
+++ codechef/medium/CSJ201AB/solution.java
@@ -0,0 +1,35 @@
+// Update the '_' in the code below to solve the problem
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		
+		int t = read.nextInt();
+		for(int i=0; i<t; i++)
+		{
+    		int N = read.nextInt();
+    		int A = read.nextInt();
+    		int B = read.nextInt();
+    		
+            if(N%A == 0 && N%B == 0){
+                System.out.println("N is divisible by A and B");
+            }
+            else if(N%A == 0){
+                System.out.println("N is divisible by only A");
+            }
+            else if(N%B == 0){
+                System.out.println("N is divisible by only B");
+            }
+            
+            // The last statement could have been an 'else' statement
+            // elif condition used to show usage of 'and' statement
+    		else if(N%A != 0 && N%B != 0){
+    		    System.out.println("N is divisible by neither A nor B");
+    		}
+		}
+	}
+}
