ff10272fde3a0bacf3de6838003af180e576fc4a
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-28 10:35:29 +0530

    ✅ BMJ204A - Rating 249 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-28

---
+++ codechef/easy/BMJ204A---Rating-249/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner read = new Scanner(System.in);
+		
+		int t = read.nextInt();
+		for(int i=0; i<t; i++)
+		
+		//Update your code below this line to solve the problem
+		{ 
+		    int x= read.nextInt();
+		    System.out.println(x*15);
+    	
+    	
+		}
+	}
+}
