5c284dc418e6a3c29d61011bdb5bf5e6892b832e
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-29 21:36:43 +0530

    ✅ LBJ201D - Rating 577 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-29

---
+++ codechef/easy/LBJ201D---Rating-577/solution.java
@@ -0,0 +1,18 @@
+// How to find the average of 2 numbers?
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    int a = 15;
+	    int c = 19;
+	    int b = (a+c)/2;
+	    System.out.println(b);
+	    
+	    a = 16;
+	    c = 18;
+	    b = (a+c)/2;
+	    System.out.println(b);
+	}
+}
