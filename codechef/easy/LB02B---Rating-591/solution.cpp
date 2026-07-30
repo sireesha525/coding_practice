ee050c1507cb88da59cf22064a78b0e199a7151a
sireesha525 <23mh1a4911@acoe.edu.in>
2026-07-30 21:55:00 +0530

    ✅ LB02B - Rating 591 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-07-30

---
+++ codechef/easy/LB02B---Rating-591/solution.cpp
@@ -0,0 +1,23 @@
+// Step 1: Read and understand the problem statement and sample test cases
+
+// Click on 'Next' once you are ready to proceed
+import java.util.Scanner;
+class solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            boolean canHaveChicken=(x%2==0);
+            boolean canHaveDuck=(y%2==0);
+            if(canHaveChicken && canHaveDuck){
+                System.out.println("ANY");
+            }
+            else if(canHaveChicken)
+            
+            
+        }
+    }
+}
