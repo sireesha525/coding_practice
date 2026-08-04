03305dd86aec11b68b056a45e957a0dd47c4a5d1
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-04 21:20:46 +0530

    ✅ LPYAS110B | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-04

--- codechef/medium/LPYAS110B/solution.py
+++ codechef/medium/LPYAS110B/solution.py
@@ -1,7 +1,10 @@
 # cook your dish here
-n=int(input())
-factorial=1
-while(n>0):
-    factorial *= n
-    n-=1
-print(factorial)
+str=input()
+ans=0
+i=0
+while i < len(str):
+    c=str[i]
+    if c=='a'or c=='e'or c=='i'or c=='o'or c=='u':
+        ans+=1
+    i+=1
+print(ans)
