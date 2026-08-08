19ad37f1def8dde3e76808dd0273610348fccb7e
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-08 22:09:52 +0530

    ✅ OBZPOG24 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/OBZPOG24/solution.cpp
@@ -0,0 +1,10 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Prints the famous movie quote using escape sequences
+        // \' is used to include a single quote within the string
+        // \" is used to include a double quote within the string
+        // \n is used to add a new line
+        // \t is used to add a tab space before "- The Shining"
+        System.out.println("Here\'s Johnny!\"\n\t- The Shining");
+    }
+}
