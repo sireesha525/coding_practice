5af2ce599757b42e539f84c79a90e3f933e329c9
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-08 22:07:39 +0530

    ✅ OBZPOG15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/OBZPOG15/solution.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Integer literal for distance in kilometers
+        System.out.println(100);
+        
+
+
+        // Float literal for distance in miles
+        System.out.println(62.14f);
+
+
+        // Double literal for distance in light years
+        System.out.println(9.4607e12);
+
+    }
+}
+
