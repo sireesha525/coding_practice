7748b68aa3d4e8ed575f14998f3f2c32efc1c941
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-08 22:12:33 +0530

    ✅ OBZPOG25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/OBZPOG25/solution.java
@@ -0,0 +1,7 @@
+class Codechef {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("The programmer exclaimed - \"It's amazing how Python handles special characters!\"");
+    }
+}
+
