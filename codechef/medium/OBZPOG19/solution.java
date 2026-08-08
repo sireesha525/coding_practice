80607c374eca35d79d883a27f39c26cf3df08357
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-08 22:08:26 +0530

    ✅ OBZPOG19 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ codechef/medium/OBZPOG19/solution.java
@@ -0,0 +1,14 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Print a character literal 'A'
+        System.out.println('A');
+        
+        // Print a string literal "John"
+        System.out.println("John");
+        
+        // Print a boolean literal true
+        System.out.println(true);
+        
+    }
+}
+
