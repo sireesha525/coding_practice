38669e33cafc2fde6e4a22e77712efc45f80ebea
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-11 21:23:26 +0530

    ✅ FYISUJ03 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/FYISUJ03/solution.java
@@ -0,0 +1,9 @@
+class Codechef  {
+    public static void main(String[] args) {
+        // Declare and assign a variable using 'var'
+        var age = 25; 
+        
+        // Print the value of the variable
+        System.out.println("My age is: " + age);
+    }
+}
