71201d7892785ab0b0f5c8b5925a5208a56f8276
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-11 21:32:35 +0530

    ✅ FYISUJ10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/FYISUJ10/solution.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main(String[] args) {
+        //  Declare and assign temperature readings
+        byte hotRegionTemp=45;
+        byte coldRegionTemp=-20;
+      
+
+
+        //  Print the temperature readings
+        System.out.println("Hot Region Temperature:"+hotRegionTemp);
+        System.out.println("Cold Region Temperature:"+coldRegionTemp);
+
+
+
+
+    }
+}
