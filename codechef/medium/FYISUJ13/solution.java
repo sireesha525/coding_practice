debeccb4535df6ad89c076f12c626b314919b512
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-11 21:33:02 +0530

    ✅ FYISUJ13 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/FYISUJ13/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign a short variable
+        short temperature = -150; 
+        
+        // Print the value of the variable
+        System.out.println("Temperature: " + temperature);
+    }
+}
