292c08866ce32ed072c6fd443a3c032101d581b0
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-11 21:27:12 +0530

    ✅ FYISUJ05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/FYISUJ05/solution.java
@@ -0,0 +1,7 @@
+ class Codechef{
+    public static void main(String[] args){
+        // Cook your dish here 
+         int age= 18;
+        System.out.println("The value stored in variable is"+age);
+    }
+}
