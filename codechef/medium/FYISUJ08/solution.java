c78abda4fd15b6e85841f801f4bd55443940a942
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-11 21:27:44 +0530

    ✅ FYISUJ08 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-11

---
+++ codechef/medium/FYISUJ08/solution.java
@@ -0,0 +1,6 @@
+class Codechef {
+    public static void main(String[] args) {
+        byte smallNumber = 100; // Declare and assign a byte variable
+        System.out.println("The byte value is: " + smallNumber); // Print the variable
+    }
+}
