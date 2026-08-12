a8c1ad6333568cefdfa5db6734bff56c55bc4b8d
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-12 10:14:30 +0530

    ✅ Count Primes | Medium | Python | Runtime: 1250 ms (61.41%) | Memory: 58 MB | 2026-08-12

---
+++ leetcode/medium/0204-count-primes/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<=2:
+            return 0
+        is_prime=[True]*n
+        is_prime[0]=is_prime[1]=False
+        p=2
+        while p*p<n:
+            if is_prime[p]:
+                for i in  range(p*p,n,p):
+                    is_prime[i]=False
+            p+=1
+        return sum(is_prime)
