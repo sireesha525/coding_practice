2313100cf90a4e58846ded9f1b482685619cbbb1
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-15 21:31:33 +0530

    ✅ FYISUJ18 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-15

---
+++ codechef/medium/FYISUJ18/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign an int variable
+        int population = 1_500_000; 
+        
+        // Print the value of the variable
+        System.out.println("City Population: " + population);
+    }
+}
