904083ce580b3f17c41f6dc48f2a70eed2770948
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-16 11:55:32 +0530

    ✅ TypeCast And Double It | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ gfg/basic/typecast-and-double-it--153103/solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        // TypeCast to int double it and print
+        int n=Integer.parseInt(num);
+        System.out.println(n*2);
+    }
+}
