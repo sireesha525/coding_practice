1e3153e057f85d5e9094527aed0470f76885b496
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:00:08 +0530

    ✅ FYISUJ25 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/FYISUJ25/solution.java
+++ codechef/medium/FYISUJ25/solution.java
@@ -1,13 +1,10 @@
-class Codechef {
-    public static void main(String[] args) {
-        // Step 3: Declare and assign speed of light
-        long speedOfLight = 299792458;  // Speed of light in meters per second
-
-        // Step 4: Declare and assign seconds in a year
-        long secondsInYear = 31536000;  // Total seconds in one year
-
-        // Step 5: Print both values
-        System.out.println("Speed of Light: " + speedOfLight+ " m/s");
-        System.out.println("Seconds in a Year: " + secondsInYear);
+ class Codechef{
+    public static void main(String[] args){
+        // Cook your dish here 
+        long transactionID =5123456789000L;
+        short branchcode = 3052;
+        System.out.println("Transaction ID:"+transactionID);
+        System.out.println("Branch Code:"+branchcode);
+        
     }
 }
