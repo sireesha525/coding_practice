e48f60163607c599cce53c45ca4f2c76abf507c2
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:10:50 +0530

    ✅ FYISUJ35 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/FYISUJ35/solution.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Step 1: Declare and assign temperature (float)
+        float temperature=28.3f;
+        
+
+
+        // Step 2: Declare and assign rainfall level (double)
+        double rainfallLevel=125.6789;
+
+        // Step 3: Print the weather data
+        System.out.println("Temperature: " + temperature+ " °C");
+        System.out.println("Rainfall Level: " +rainfallLevel + " mm");
+
+    }
+}
