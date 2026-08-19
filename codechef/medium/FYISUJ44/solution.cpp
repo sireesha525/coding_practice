1d9358a09451c60810657a4c9e8dc6e357912506
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:19:56 +0530

    ✅ FYISUJ44 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/FYISUJ44/solution.cpp
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign a boolean variable
+        boolean isLightOn = true; 
+        
+        // Print the value of the variable
+        System.out.println("Is the light on? " + isLightOn);
+    }
+}
