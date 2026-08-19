52edd998ea2a0b2e86456cda227bdaae1f8b56af
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:16:41 +0530

    ✅ FYISUJ40 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/FYISUJ40/solution.java
@@ -0,0 +1,12 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign mission details
+        char missionCode='A';
+        short crewSize=7;
+        long distanceTraveled=3000000000L;
+        System.out.println("Mission Code:"+missionCode);
+        System.out.println("Crew Size:"+crewSize);
+        System.out.println("Distance Traveled:"+distanceTraveled+"km");
+
+    }
+}
