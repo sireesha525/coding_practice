4971c8a2d873d8139cea13d0ecec4318b9ca1d3b
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:11:31 +0530

    ✅ FYISUJ38 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/FYISUJ38/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign a char variable
+        char grade = 'A'; 
+        
+        // Print the value of the variable
+        System.out.println("Student Grade: " + grade);
+    }
+}
