183423dd5963019977acfc7310d9cf45f873dc13
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:00:54 +0530

    ✅ FYISUJ28 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/FYISUJ28/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign a float variable
+        float averageTemperature = 28.75f; 
+        
+        // Print the value of the variable
+        System.out.println("Average Temperature: " + averageTemperature + "°C");
+    }
+}
