dfa088062e2ed89e5fb1eed9e3cb5ce2185d4c90
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-19 20:07:45 +0530

    ✅ FYISUJ30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/FYISUJ30/solution.java
+++ codechef/medium/FYISUJ30/solution.java
@@ -1,11 +1,14 @@
 class Codechef {
     public static void main(String[] args) {
-        // Step 3: Declare and assign rating values
-        float currentRating = 4.7f;  // Current rating of the video
-        float maxRating = 5.0f;  // Maximum possible rating
+        //  Declare and assign values
+        float fuelEfficiency =18.5f;
+        int distanceCovered = 150;
+        System.out.println("Fuel Efficiency:"+fuelEfficiency+"km/l");
+        System.out.println("Distance Covered:"+distanceCovered+"km");
 
-        // Step 5: Print the ratings
-        System.out.println("Current Video Rating: " + currentRating);
-        System.out.println("Maximum Possible Rating: " + maxRating);
+
+        //  Print the values
+
+
     }
 }
