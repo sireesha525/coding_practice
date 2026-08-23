679fc952c23a2ff769017d64d698b0418c02b321
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-23 11:23:45 +0530

    ✅ KITCHENTIME - Rating 273 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/easy/KITCHENTIME---Rating-273/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc= new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    System.out.println(y-x);
+		}
+	}
+}
