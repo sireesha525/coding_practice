3e73e4d1d8b843281458199eb4683f662d3ab41a
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-23 11:29:49 +0530

    ✅ AUDIBLE - Rating 273 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ codechef/easy/AUDIBLE---Rating-273/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    if(n<=m){
+		        System.out.println(0);
+		    }
+		    else{
+		        System.out.println(n-m);		    }
+		}
+
+	}
+}
