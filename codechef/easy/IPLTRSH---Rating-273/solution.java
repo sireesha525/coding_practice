fdf4934cf2a53648ff802ba1c972e2642ec588e6
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-23 11:29:47 +0530

    ✅ IPLTRSH - Rating 273 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-23

--- codechef/easy/IPLTRSH---Rating-273/solution.java
+++ codechef/easy/IPLTRSH---Rating-273/solution.java
@@ -4,15 +4,20 @@ import java.io.*;
 
 class Codechef
 {
-	public static void main (String[] args) 
+	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc= new Scanner (System.in);
+		Scanner sc=new Scanner (System.in);
 		int t=sc.nextInt();
 		while(t-->0){
-		    int x=sc.nextInt();
-		    int y=sc.nextInt();
-		    System.out.println(y-x);
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    if(n<=m){
+		        System.out.println(0);
+		    }
+		    else{
+		        System.out.println(n-m);		    }
 		}
+
 	}
 }
