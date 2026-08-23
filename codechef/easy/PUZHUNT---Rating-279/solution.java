98027ba8273a491957c279e8a069135a88d03f8e
sireesha525 <23mh1a4911@acoe.edu.in>
2026-08-23 11:48:04 +0530

    ✅ PUZHUNT - Rating 279 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-23

--- codechef/easy/PUZHUNT---Rating-279/solution.java
+++ codechef/easy/PUZHUNT---Rating-279/solution.java
@@ -8,7 +8,7 @@ class Codechef
 	{
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
-	    if(n>=6 && n<=8){
+	    if(n>=10){
 	        System.out.println("YES");
 	    }
 	    else{
