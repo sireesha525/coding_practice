58d5266644c9f76bc6ee65df84c291c9e970d104
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-01 12:09:34 +0530

    ✅ NOGJPU01 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/medium/NOGJPU01/solution.java
@@ -0,0 +1,10 @@
+class ArcadeHighScore {
+    public static void main(String[] args) {
+        String playerName="RetroGamer";
+        int highScore =50000;
+        System.out.println("Player: " + playerName + ", Initial High Score: " + highScore);
+        highScore=75000;
+        System.out.println("Player: " + playerName + ", New High Score: " + highScore);
+        
+    }
+}
