3dedcb4592fdda47973b44361177f5e983b24b99
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-02 16:01:53 +0530

    ✅ Concatenate Integers | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ gfg/basic/concatenate-integers/solution.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // code here
+        System.out.print(""+a+b);
+    }
+}
