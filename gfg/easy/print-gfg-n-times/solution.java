4d9de22a72a94773e9e78cc769802f9a438bd000
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-02 15:57:22 +0530

    ✅ Print GFG n times | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ gfg/easy/print-gfg-n-times/solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i=0;i<n;i++){
+            System.out.print("GFG"+" ");
+        }
+    }
+}
