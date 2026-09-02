e575971e6f7d51c141e3ead2b1f508a3a80a2a63
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-02 15:16:32 +0530

    ✅ CRQDWE05 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/CRQDWE05/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Write your code here
+        int pushups = 40, situps = 30, squats = 20;
+        int totalExercises=pushups + situps + squats;
+        System.out.println(totalExercises);
+        
+    }
+}
