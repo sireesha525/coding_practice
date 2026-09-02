1a5eb34ed7adbd4b36427f95532deed498eb5653
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-02 15:39:27 +0530

    ✅ CRQDWE15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/CRQDWE15/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args) {
+
+         // Given Variables
+        int sponsors = 4, donationPerSponsor = 4500, extraDonations = 8000;
+        int totalFunds = (sponsors * donationPerSponsor) + extraDonations;
+        System.out.println(totalFunds);
+    }
+}
