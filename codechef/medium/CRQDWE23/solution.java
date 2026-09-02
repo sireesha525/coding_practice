5718938351c9df358618c04bdc7fdd92a03fef1e
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-02 15:50:59 +0530

    ✅ CRQDWE23 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/CRQDWE23/solution.java
@@ -0,0 +1,18 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare integer variables
+        int num1 = 20;
+        int num2 = 6;
+        int num3 = 15;
+        int num4 = 4;
+
+        // Perform modulus operation
+        int remainder1 = num1 % num2; // 20 % 6 = 2
+        int remainder2 = num3 % num4; // 15 % 4 = 3
+
+        // Print results
+        System.out.println("Remainder when 20 is divided by 6 : " + remainder1);
+        System.out.println("Remainder when 15 is divided by 4 : " + remainder2);
+    }
+}
+
