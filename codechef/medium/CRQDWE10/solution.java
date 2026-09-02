34b943524d7b2f81fcd7f19cabe305868580c155
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-02 15:32:17 +0530

    ✅ CRQDWE10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/CRQDWE10/solution.java
@@ -0,0 +1,11 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare integer variables for poster inventory
+        int totalPosters = 200, removedPosters = 75, newPosters = 40;
+        // Calculate the updated stock
+        int updatedStock = totalPosters - removedPosters + newPosters;
+
+        // Print the updated stock
+        System.out.println(updatedStock);
+    }
+}
