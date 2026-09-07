f05b7573801b6e836ed81b004a5c5d009e2a36b3
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-08 05:22:57 +0530

    ✅ While Loop | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ gfg/basic/while-loop/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    // code here
+    for(int i=x;i>=0;i--){
+        cout << i << " ";
+    }
+    
+
+    return 0;
+}
