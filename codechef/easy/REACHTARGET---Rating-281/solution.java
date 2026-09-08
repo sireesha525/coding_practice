1c57a9cb95daa295feb2e156ff0bb317a50c3334
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-09 05:22:45 +0530

    ✅ REACHTARGET - Rating 281 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ codechef/easy/REACHTARGET---Rating-281/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner sc= new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int ans=x-y;
+		    System.out.println(x-y);
+		}
+	}
+}
