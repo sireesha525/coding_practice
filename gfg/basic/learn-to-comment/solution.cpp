637604e2c6ef9b2d1a59a87d0030383e6d6b08d1
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-08 05:40:08 +0530

    ✅ Learn to Comment | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ gfg/basic/learn-to-comment/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+
+    // Prints a
+    cout << a << endl;
+
+    // Prints b
+    //cout << b << endl;
+
+    // Prints c
+    cout << c << endl;
+}
