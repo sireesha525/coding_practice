5fc0fdb36491834a355a1b018e4c8eb31fba69da
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-08 05:37:04 +0530

    ✅ The If Statement | Basic | C++ | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ gfg/basic/the-if-statement--113256/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin >> n;
+    if(n > 100){
+        cout << "Big" << endl;
+        }
+    cout << "Number" << endl;
+    return 0;
+}
+
+
