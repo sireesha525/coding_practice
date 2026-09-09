4742a1d6329f7e1b9159f2b7102da4942e7d3b09
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-10 04:49:58 +0530

    ✅ MINHEIGHT - Rating 285 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/easy/MINHEIGHT---Rating-285/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int h=sc.nextInt();
+		    if(x>=h){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
