817be114dccc08f12e48c985afedbdd1929d5b95
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-11 04:48:37 +0530

    ✅ BTRYHLTH - Rating 296 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-10

--- codechef/easy/BTRYHLTH---Rating-296/solution.java
+++ codechef/easy/BTRYHLTH---Rating-296/solution.java
@@ -8,11 +8,15 @@ class Codechef
 	{
 		// your code goes here
 		Scanner sc=new Scanner (System.in);
-		int t =sc.nextInt();
+		int t = sc.nextInt();
 		while(t-->0){
-		   int n=sc.nextInt();
-		   int m=sc.nextInt();
-		   System.out.println(n*m);
+		    int x=sc.nextInt();
+		    if(x>=80){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
 		}
 
 	}
