c09d683ba2360243e5d7c7c5d430ae7f22b365b5
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-11 05:41:40 +0530

    ✅ The FizzBuzz Program | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ gfg/easy/the-fizzbuzz-program/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    if(n%3==0 && n%5==0){
+        cout<<"FizzBuzz";
+    }
+    else if(n%3==0){
+        cout<<"Fizz";
+    }
+    else if(n%5==0){
+        cout<<"Buzz";
+    }
+    else{
+        cout<<n;
+    }
+
+    return 0;
+}
