88ba2adb15613a01beb9f440e3abd42794c0f7a5
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-13 11:02:49 +0530

    ✅ SYNMCQ10 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/medium/SYNMCQ10/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a = -50;
+    int b = 40;
+    // Update your code below this line
+    int sum=a+b;
+    cout<<sum<<endl;
+    int product=a*b;
+    cout<<product<<endl;
+    int quotient =a/b;
+    cout<<quotient;
+ 
+
+
+
+
+  
+  
+}
