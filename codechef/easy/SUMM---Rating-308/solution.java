953ccdc8eeb0042606b1e9018849306d5922a5bd
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-13 05:50:17 +0530

    ✅ SUMM - Rating 308 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/easy/SUMM---Rating-308/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if(a+b==c){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+		
+
+	}
+}
