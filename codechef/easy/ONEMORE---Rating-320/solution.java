0e07b1b19a3b528a367fb89d89c0ae418a7cdc02
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-14 05:52:22 +0530

    ✅ ONEMORE - Rating 320 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/easy/ONEMORE---Rating-320/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    if(x<=24){
+		        System.out.println("NO");
+		    }
+		    else{
+		        System.out.println("YES");
+		    }
+		}
+
+	}
+}
