14e4123851d4ffd99212f8c295e76bd1de3d0c65
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-15 04:41:17 +0530

    ✅ FINE - Rating 330 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-14

---
+++ codechef/easy/FINE---Rating-330/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if(a>b && a>c){
+		        System.out.println("Alice");
+		    }
+		    else if (b>a && b>c){
+		        System.out.println("Bob");
+		    }
+		    else{
+		        System.out.println("Charlie");
+		    }
+		    
+		}
+
+	}
+}
