d85723ab60cc89546f4a97f022f98962cd201079
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-15 04:36:30 +0530

    ✅ AUCTION - Rating 328 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/easy/AUCTION---Rating-328/solution.java
+++ codechef/easy/AUCTION---Rating-328/solution.java
@@ -1,28 +1,26 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import java.util.Scanner;
 
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc=new Scanner (System.in);
+		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt();
-		for(int i=0; i<t;i++){
+		while(t-->0){
 		    int x=sc.nextInt();
-		if(x<3){
-		    System.out.println("LIGHT");
+		    if(x<3){
+		        System.out.println("LIGHT");
+		    }
+		    else if(x>=3 && x<7){
+		        System.out.println("MODERATE");
+		    }
+		    else{
+		        System.out.println("HEAVY");
+		    }
 		}
-		else if(x<7){
-		    System.out.println("MODERATE");
-		}
-		else{
-		    System.out.println("HEAVY");
-		}                                                         
-	}  
-	sc.close();
 
 	}
 }
