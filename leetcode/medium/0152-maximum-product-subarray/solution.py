981ba5ca167c973cac374951c7aa79f5d6c6522c
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-15 05:24:38 +0530

    ✅ Maximum Product Subarray | Medium | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-09-14

--- leetcode/medium/0152-maximum-product-subarray/solution.py
+++ leetcode/medium/0152-maximum-product-subarray/solution.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res=max(nums)
-        curMin=1
-        curMax=1
-        for n in nums:
-            if n==0:
-                curMin=1
-                curMax=1
-                continue
-            temp = curMax
-            curMax=max(n * curMax,n * curMin,n)
-            curMin=min(n * curMax,n * curMin,n)
-            res=max(res,curMax)
-        return res
+        res = max(nums)
+        curMin = 1
+        curMax = 1
 
-        
+        for n in nums:
+            if n == 0:
+                curMin = 1
+                curMax = 1
+                continue
+
+            tempMax = curMax
+
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(n * tempMax, n * curMin, n)
+
+            res = max(res, curMax)
+
+        return res
