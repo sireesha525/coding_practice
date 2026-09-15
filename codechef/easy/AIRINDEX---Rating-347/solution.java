ed8c613364d9a5deb47db6b552846c3ea365e6b4
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-16 04:38:32 +0530

    ✅ AIRINDEX - Rating 347 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/easy/AIRINDEX---Rating-347/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int x=sc.nextInt();
+		if(x<=100){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
