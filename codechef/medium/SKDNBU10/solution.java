b94e1a9d638baa60db2b2ff9f82b97eebc2f80d4
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-15 20:23:30 +0530

    ✅ SKDNBU10 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/SKDNBU10/solution.java
@@ -0,0 +1,9 @@
+class Codechef {
+    public static void main(String[] args){
+        
+        double accountBalance = 5000.50, interest = 250.75;
+        // Cook your dish here
+        accountBalance += interest;
+        System.out.println(accountBalance);
+    }
+}
