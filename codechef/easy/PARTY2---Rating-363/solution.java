50b7fb3f072abdd7185de269b5a58f03352fd861
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-17 04:55:43 +0530

    ✅ PARTY2 - Rating 363 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/easy/PARTY2---Rating-363/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int x=sc.nextInt();
+		    int k=sc.nextInt();
+		    if(n * x > k){
+		        System.out.println("NO");
+		    }
+		    else{
+		        System.out.println("YES");
+		    }
+		}
+
+	}
+}
