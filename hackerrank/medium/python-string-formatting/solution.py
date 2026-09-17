873be2c5ace41750d28f6ef07b3c9002c6768152
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-17 11:31:22 +0530

    ✅ String Formatting | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-17

--- hackerrank/medium/python-string-formatting/solution.py
+++ hackerrank/medium/python-string-formatting/solution.py
@@ -1,3 +1,14 @@
-def mutate_string(string, position, character):
-    return string[:position] + character + string[position + 1:]
+def print_formatted(number):
+    # your code goes here
+    width = len(bin(n)[2:])
+
+    for i in range(1, n + 1):
+        print(
+            str(i).rjust(width),
+            oct(i)[2:].rjust(width),
+            hex(i)[2:].upper().rjust(width),
+            bin(i)[2:].rjust(width)
+        )
+
+
 
