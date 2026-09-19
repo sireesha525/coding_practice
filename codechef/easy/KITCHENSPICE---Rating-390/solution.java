d231cd254b2a74c732b087fd9b7947641883b6fa
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-19 11:41:51 +0530

    ✅ KITCHENSPICE - Rating 390 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-19

---
+++ codechef/easy/KITCHENSPICE---Rating-390/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    if(x<4){
+		        System.out.println("MILD");
+		    }
+		    else if(x<7){
+		        System.out.println("MEDIUM");
+		    }
+		    else{
+		        System.out.println("HOT");
+		    }
+		}
+
+	}
+}
