0ab11adc5c4fabf4283578a934a91fe424f2f21f
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-21 05:41:40 +0530

    ✅ CABS - Rating 399 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/easy/CABS---Rating-399/solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(x<y){
+		        System.out.println("FIRST");
+		    }
+		    else if(x>y){
+		        System.out.println("SECOND");
+		    }
+		    else{
+		        System.out.println("ANY");
+		    }
+		    
+		}
+
+	}
+}
