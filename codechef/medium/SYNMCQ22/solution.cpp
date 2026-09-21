157267461f93936cf6f7f15c032effb94d647a77
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-21 05:58:06 +0530

    ✅ SYNMCQ22 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/SYNMCQ22/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code as per given instructions on the left
+    int a, b;
+    int sum;
+    int diff;
+    cin>>a;
+    cin>>b;
+    sum = a + b;
+    diff = a - b;
+    cout << "Sum is: " << sum << endl;
+    cout << "Difference is: "<< diff << endl;
+}
