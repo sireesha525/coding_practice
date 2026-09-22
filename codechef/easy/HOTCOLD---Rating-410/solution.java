20619e782375afbf035e0a5b95e7c5e4f59b2a0a
sireesha525 <23mh1a4911@acoe.edu.in>
2026-09-22 22:03:42 +0530

    ✅ HOTCOLD - Rating 410 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-22

--- codechef/easy/HOTCOLD---Rating-410/solution.java
+++ codechef/easy/HOTCOLD---Rating-410/solution.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -7,19 +6,18 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner scanner = new Scanner(System.in);
-        int X = scanner.nextInt();
-        double Y = scanner.nextDouble();
+		// your code goes here
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int c=sc.nextInt();
+		    if(c>20){
+		        System.out.println("HOT");
+		    }
+		    else{
+		        System.out.println("COLD");
+		    }
+		}
 
-        // Check if the withdrawal amount is a multiple of 5 
-        // and if there is enough balance for the amount plus the 0.50 charge.
-        if (X % 5 == 0 && X + 0.50 <= Y) {
-            Y -= (X + 0.50);
-        }
-
-        // Print the final balance with exactly two decimal places.
-        System.out.printf("%.2f\n", Y);
 	}
 }
-
-	
